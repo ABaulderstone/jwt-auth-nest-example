bd8a68cc1f50b94a86042c7b630974a0aaae35a1
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-11-22 16:34:26 +1100

    changes jwtservice register to async

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,18 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MikroOrmModule.forRoot(),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
@@ -7,9 +8,14 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      secret: 'test',
-      signOptions: { expiresIn: '20 minutes' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '20 minutes' },
+        };
+      },
     }),
   ],
   providers: [AuthService],
