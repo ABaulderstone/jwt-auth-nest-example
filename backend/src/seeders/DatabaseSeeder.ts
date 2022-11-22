e89dd75aa4d92b8769441ad44a427a62a566e40c
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-11-22 15:17:53 +1100

    adds password hashing

--- backend/src/seeders/DatabaseSeeder.ts
+++ backend/src/seeders/DatabaseSeeder.ts
@@ -1,12 +1,14 @@
 import type { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
 import { User } from 'src/users/entities/user.entity';
+import { generateHash } from '../shared/password';
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
+    const hashedPassword = await generateHash('password1');
     const user = em.create(User, {
       email: 'test@test.com',
-      password: 'password1',
+      password: hashedPassword,
       createdAt: new Date(),
       updatedAt: new Date(),
     });
---
+++ backend/src/shared/password.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from 'bcrypt';
+
+export const generateHash = async (password: string): Promise<string> => {
+  const saltRounds = 12;
+
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(password, salt);
+};
+
+export const validateHash = async (
+  incoming: string,
+  hash: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(incoming, hash);
+};
