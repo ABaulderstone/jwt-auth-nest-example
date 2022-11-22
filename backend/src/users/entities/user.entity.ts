cf9aea7caf2bda7714a6f1c82095c5acd8253415
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-11-22 14:27:13 +1100

    adds base and user entity

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { Entity, Property } from '@mikro-orm/core';
+import { BaseEntity } from 'src/shared/base.entity';
+
+@Entity({ tableName: 'users' })
+export class User extends BaseEntity {
+  @Property()
+  email: string;
+
+  @Property()
+  password: string;
+}
